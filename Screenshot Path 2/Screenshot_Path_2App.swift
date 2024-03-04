1a97890db394d7eeb91c4bcf8e99a0ad330052b8
Iomegan <dwitt.kiel@icloud.com>
2024-03-04 12:01:38 +0100

    Initial Commit

---
+++ Screenshot Path 2/Screenshot_Path_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Screenshot_Path_2App.swift
+//  Screenshot Path 2
+//
+//  Created by Daniel Witt on 04.03.24.
+//
+
+import SwiftUI
+
+@main
+struct Screenshot_Path_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
